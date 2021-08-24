bd4654bcbab61f88a8dbd520926dbad3583411f6
vjs22334 <vjs122334@gmail.com>
2021-08-24 23:09:00 +0530

    added basic line drawing and follow

---
+++ Assets/Scripts/PathDrawer.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(LineRenderer))]
+public class PathDrawer : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public float pointSpacing = 0.5f;
+
+    public LayerMask MouseRayCastLayerMask;
+    List<Vector3> positions;
+    Vector3 currMousePosition;
+
+    bool drawing = false;
+    ship Ship = null;
+
+    Camera camera;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        camera = Camera.main;
+        positions = new List<Vector3>();
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        Vector3 currMousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+        currMousePosition.z=0;
+        
+        if(Input.GetButtonDown("Fire1")){
+
+            Collider2D shipCollider  = Physics2D.OverlapPoint(currMousePosition,MouseRayCastLayerMask);
+            if(shipCollider!=null){
+                drawing = true;
+                Ship = shipCollider.GetComponent<ship>();
+            }
+        }
+
+        if(Input.GetButton("Fire1") && drawing){
+            
+            if(DistanceFromLastpoint(currMousePosition) > pointSpacing){
+                positions.Add(currMousePosition);
+                lineRenderer.positionCount = positions.Count;
+                lineRenderer.SetPositions(positions.ToArray());
+                Ship.SetNewPath(positions.ToArray());
+            }
+        }
+
+        if(Input.GetButtonUp("Fire1") && drawing){
+            drawing = false;
+            positions.Clear();
+            lineRenderer.positionCount = positions.Count;
+        }
+    }
+
+    float DistanceFromLastpoint(Vector3 point){
+        if(positions.Count == 0){
+            return float.MaxValue;
+        }
+        return  (point - positions[positions.Count-1]).magnitude;
+    }
+}
---
+++ Assets/ship.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(LineRenderer))]
+public class ship : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float turnspeed = 100f;
+
+    public Transform spriteTransform;
+
+    LineRenderer lineRenderer;
+    List<Vector3> pathPoints;
+
+    int currPathIndex = 1;
+
+    Vector3 currDirection;
+
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        pathPoints = new List<Vector3>();
+        currDirection = transform.up;
+    }
+
+    public void SetNewPath(Vector3[] newPoints){
+        pathPoints = new List<Vector3>(newPoints);
+        lineRenderer.positionCount = pathPoints.Count;
+        lineRenderer.SetPositions(pathPoints.ToArray());
+    }
+
+    void clearpath(){
+        pathPoints.Clear();
+        lineRenderer.positionCount = 0;
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if(pathPoints.Count > 1){
+            if(currPathIndex > pathPoints.Count-1){
+                currPathIndex = 1;
+                clearpath();
+            }
+
+            currDirection = (pathPoints[currPathIndex] - transform.position).normalized;
+        }
+        else{
+            currPathIndex = 1;
+            clearpath();
+        }
+
+        transform.position += currDirection*moveSpeed*Time.deltaTime;
+        float angleDiff = Vector3.SignedAngle(spriteTransform.up,currDirection,Vector3.forward);
+        float zRotation = Mathf.Sign(angleDiff)*turnspeed*Time.deltaTime;
+        zRotation = Mathf.Clamp(zRotation,-1*Mathf.Abs(angleDiff),Mathf.Abs(angleDiff));
+        spriteTransform.localRotation = Quaternion.Euler(0,0,spriteTransform.localRotation.z+zRotation);
+        
+        if((currPathIndex < pathPoints.Count)&&(pathPoints[currPathIndex] - transform.position).magnitude < 0.01f)
+            currPathIndex++;
+    }
+
+
+
+}
