f2949a8b589dbe326af0246ee6373e06e057e67e
vjs22334 <vjs122334@gmail.com>
2021-08-26 23:29:45 +0530

    basic ship collision and landing done

--- Assets/Scripts/ship.cs
+++ Assets/Scripts/ship.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 [RequireComponent(typeof(LineRenderer))]
 public class ship : MonoBehaviour
 {
@@ -8,12 +9,16 @@ public class ship : MonoBehaviour
 
     public Transform spriteTransform;
 
+    public LandZone landZone;
+
     LineRenderer lineRenderer;
     List<Vector3> pathPoints;
 
     int currPathIndex = 1;
 
     Vector3 currDirection;
+
+    bool Landed = false;
 
 
     /// <summary>
@@ -45,6 +50,10 @@ public class ship : MonoBehaviour
     /// </summary>
     void Update()
     {
+        if(Landed){
+            return;
+        }
+
         if (pathPoints.Count > 1)
         {
             if (currPathIndex > pathPoints.Count - 1)
@@ -54,6 +63,8 @@ public class ship : MonoBehaviour
             }
 
             currDirection = (pathPoints[currPathIndex] - transform.position).normalized;
+            currDirection.z = 0;
+            currDirection = currDirection.normalized;
         }
         else
         {
@@ -62,21 +73,63 @@ public class ship : MonoBehaviour
         }
 
         transform.position += currDirection * moveSpeed * Time.deltaTime;
-        float angleDiff = Vector3.SignedAngle(spriteTransform.up, currDirection, Vector3.forward);
+        float angleDiff = Vector2.SignedAngle(spriteTransform.up, currDirection);
         float zRotation = Mathf.Sign(angleDiff) * turnspeed * Time.deltaTime;
+        // Debug.Log("zRotation before:"+zRotation);
         zRotation = Mathf.Clamp(zRotation, -1 * Mathf.Abs(angleDiff), Mathf.Abs(angleDiff));
-        spriteTransform.localRotation = Quaternion.Euler(0, 0, spriteTransform.localRotation.z + zRotation);
-
+        // Debug.Log("AngleDiff:"+angleDiff);
+        // Debug.Log("zRotation:"+zRotation);
+        // Debug.DrawRay(transform.position,currDirection*3,Color.green);
+        // Debug.Log("oldZ: "+ spriteTransform.localRotation.eulerAngles.z);
+        float newZ = spriteTransform.localRotation.eulerAngles.z + zRotation;
+        // Debug.Log("newZ: "+ newZ);
+        spriteTransform.localRotation = Quaternion.Euler(0, 0, newZ);
+        // Debug.DrawRay(transform.position,spriteTransform.up*3,Color.red);
         if ((currPathIndex < pathPoints.Count) && (pathPoints[currPathIndex] - transform.position).magnitude < 0.01f)
             currPathIndex++;
+
     }
 
-    public void SetSpawnSpeedAndRot(float MoveSpeed, float TurnRot)
-    {
-        moveSpeed = MoveSpeed;
-        turnspeed = TurnRot;
+    public void LandingProcess(Vector2 landDirection, Vector3 landPosition){
+        if(Landed){
+            return;
+        }
+        Landed = true;
+        clearpath();
+        transform.DOScale(new Vector3(0.6f,0.6f,0.6f),1f);
+        float angleDiff = Vector2.SignedAngle(spriteTransform.up, landDirection);
+        Debug.Log(angleDiff);
+        float newZ = transform.localRotation.eulerAngles.z + angleDiff;
+        spriteTransform.DOLocalRotate(new Vector3(0,0,newZ),1f);
+        transform.DOMove(landPosition,1f).onComplete += ()=>{
+            //do runway animation. for now I'm making it dissapear.
+            DestroyShip();
+        };
     }
 
+    public void DestroyShip(){
+        //Do vfx here.
+        Destroy(gameObject);
+    }
+
+    //color change to indicate going to land
+    public void HeadingToLand(){
+
+    }
+
+    //collision indicator
+    public void CollisionAlert(){
+
+    }
+
+
+    public void ResetHeadingToLand(){
+
+    }
+
+    public void ResetCollisionAlert(){
+
+    }
 
 
 }
