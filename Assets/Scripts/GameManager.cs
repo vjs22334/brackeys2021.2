fd8736dbb8220dd240c5a1bc6e1c71d3f05879e1
thenerdpoint60 <thenerdpoint60@gmail.com>
2021-08-27 20:29:14 +0530

    [Added]Prototype level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,5 @@
-using System;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,8 +12,8 @@ public class GameManager : MonoBehaviour
     public int SCorePerKill = 1;
 
     [Header("UI References")]
-    public Text ScoreText;
-    public Text LivesText;
+    public TMP_Text ScoreText;
+    public TMP_Text LivesText;
 
     public GameObject PauseUI;
     public GameObject GameOverUI;
@@ -26,22 +25,28 @@ public class GameManager : MonoBehaviour
 
 
     private static GameManager _instance = null;
-    public static GameManager Instance{
-        get{
-            if(_instance == null){
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
                 _instance = FindObjectOfType<GameManager>();
             }
             return _instance;
         }
     }
-    void Awake(){
-        if(_instance == null){
+    void Awake()
+    {
+        if (_instance == null)
+        {
             _instance = this;
         }
-        else if(_instance != this){
+        else if (_instance != this)
+        {
             Destroy(this.gameObject);
         }
-    }        
+    }
 
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
@@ -57,44 +62,55 @@ public class GameManager : MonoBehaviour
         isPlaying = true;
     }
 
-    public void Pause(){
+    public void Pause()
+    {
         Time.timeScale = 0;
         PauseUI.SetActive(true);
     }
 
-    public void Resume(){
+    public void Resume()
+    {
         Time.timeScale = 1;
         PauseUI.SetActive(false);
     }
 
-    void UpdatelivesUI(){
+    void UpdatelivesUI()
+    {
         LivesText.text = lives.ToString();
     }
 
-    void updateScoreUI(){
+    void updateScoreUI()
+    {
         ScoreText.text = score.ToString();
     }
 
-    public void AddScore(bool landing){
-        if(!isPlaying){
+    public void AddScore(bool landing)
+    {
+        if (!isPlaying)
+        {
             return;
         }
-        if(landing){
+        if (landing)
+        {
             score += ScorePerLanding;
         }
-        else{
+        else
+        {
             score += SCorePerKill;
         }
         updateScoreUI();
     }
 
-    public void RemoveLife(){
-        if(lives == 0 || !isPlaying){
+    public void RemoveLife()
+    {
+        if (lives == 0 || !isPlaying)
+        {
             return;
         }
         lives--;
         UpdatelivesUI();
-        if(lives == 0){
+        if (lives == 0)
+        {
             GameOver();
         }
     }
