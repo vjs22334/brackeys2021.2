ccfe6fce54977643476229f595679798850847b6
thenerdpoint60 <thenerdpoint60@gmail.com>
2021-08-28 18:03:52 +0530

    [Fixed] Enemy Spawning Logic

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,49 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : ship
 {
 
-    public float MaxDirectionChangeTime;
+    //public float MaxDirectionChangeTime;
 
-    public float MinDirectionChangeTime;
-    
+    //public float MinDirectionChangeTime;
+
 
     protected override void Start()
     {
         //currDirection = transform.up;
-        StartCoroutine(ChangeDirection());
+        //StartCoroutine(ChangeDirection());
     }
 
     protected override void Update()
     {
         transform.position += currDirection * moveSpeed * Time.deltaTime;
-        float angleDiff = Vector2.SignedAngle(spriteTransform.up, currDirection);
-        if(Mathf.Abs(angleDiff) > 1f){
-            float zRotation = Mathf.Sign(angleDiff) * turnspeed * Time.deltaTime;
-            zRotation = Mathf.Clamp(zRotation, -1 * Mathf.Abs(angleDiff), Mathf.Abs(angleDiff));
-            float newZ = spriteTransform.localRotation.eulerAngles.z + zRotation;
-            spriteTransform.localRotation = Quaternion.Euler(0, 0, newZ);
+        float anglediff = Vector2.SignedAngle(spriteTransform.up, currDirection);
+        if (Mathf.Abs(anglediff) > 1f)
+        {
+            float zrotation = Mathf.Sign(anglediff) * turnspeed * Time.deltaTime;
+            zrotation = Mathf.Clamp(zrotation, -1 * Mathf.Abs(anglediff), Mathf.Abs(anglediff));
+            float newz = spriteTransform.localRotation.eulerAngles.z + zrotation;
+            spriteTransform.localRotation = Quaternion.Euler(0, 0, newz);
         }
     }
 
     /// <summary>
     /// This function is called when the MonoBehaviour will be destroyed.
     /// </summary>
-    void OnDestroy()
-    {
-        StopCoroutine(ChangeDirection());
-    }
+    //void OnDestroy()
+    //{
+    //    StopCoroutine(ChangeDirection());
+    //}
 
-    IEnumerator ChangeDirection(){
-        while(true)
-        {
-            float waitTime = Random.Range(MinDirectionChangeTime,MaxDirectionChangeTime);
-            yield return new WaitForSeconds(waitTime);
-            
-            Vector2 newDirection = Random.insideUnitCircle;
-            currDirection = new Vector3(newDirection.x,newDirection.y,0);
-        }
-    }
+    //IEnumerator ChangeDirection()
+    //{
+    //    while (true)
+    //    {
+    //        float waitTime = Random.Range(MinDirectionChangeTime, MaxDirectionChangeTime);
+    //        yield return new WaitForSeconds(waitTime);
+
+    //        Vector2 newDirection = Random.insideUnitCircle;
+    //        currDirection = new Vector3(newDirection.x, newDirection.y, 0);
+    //    }
+    //}
 }
