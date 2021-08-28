2191d5674b84be027ab17d7ca9103a3470778330
vjs22334 <vjs122334@gmail.com>
2021-08-28 13:30:57 +0530

    added arrowpointer script

--- Assets/Scripts/ArrowPointer.cs
+++ Assets/Scripts/ArrowPointer.cs
@@ -1,18 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey.Utils;
 
 public class ArrowPointer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform targetTransform;
+    
+
+    public float offset = 200f;
+
+    [SerializeField]
+    private Camera UICamera;
+    
+    RectTransform rectTransform;
+
+    Image image;
+
+    Camera mainCam;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
     void Start()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
+        mainCam = Camera.main;
+        UICamera = GameObject.FindGameObjectWithTag("Uicamera").GetComponent<Camera>();
+
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
     void Update()
     {
-        
+        if(targetTransform!=null){
+            Vector3 fromPosition = mainCam.transform.position;
+            Vector3 toPosition = targetTransform.position;
+            fromPosition.z=0;
+            Vector3 dir = (toPosition-fromPosition).normalized;
+
+            float angle = UtilsClass.GetAngleFromVector(dir);
+
+            rectTransform.localEulerAngles = new Vector3(0,0,angle);
+
+            Vector3 targetScreenPosition = mainCam.WorldToScreenPoint(toPosition);
+            bool isOffscreen = targetScreenPosition.x<=offset || targetScreenPosition.x>=Screen.width-offset || targetScreenPosition.y<=offset|| targetScreenPosition.y>=Screen.height-offset;
+
+            if(isOffscreen){
+                image.enabled = true;
+                Vector3 cappedTargetPosition = targetScreenPosition;
+                if(cappedTargetPosition.x <= offset){
+                    cappedTargetPosition.x = offset;
+                }
+                if(cappedTargetPosition.x >= Screen.width-offset){
+                    cappedTargetPosition.x = Screen.width-offset;
+                }
+                if( cappedTargetPosition.y <= offset){
+                    cappedTargetPosition.y = offset;
+                }
+                if(cappedTargetPosition.y>=Screen.height-offset){
+                    cappedTargetPosition.y = Screen.height-offset;
+                }
+
+                Vector3 pointerWorldPosition = UICamera.ScreenToWorldPoint(cappedTargetPosition);
+                rectTransform.position = pointerWorldPosition;
+                rectTransform.localPosition = new Vector3(rectTransform.localPosition.x,rectTransform.localPosition.y,0);
+            }
+            else{
+                image.enabled = false;
+            }
+
+        }
+       
+    
     }
 }
